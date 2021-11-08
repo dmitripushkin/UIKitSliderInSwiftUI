ea1e5ecd1a64db0b381d98c0c30ada576c36a7f4
Dmitri Gordeev <dimapushkin@gmail.com>
2021-11-09 00:27:59 +0300

    Done with UISlider and DataFlow between Slider and ContentView

--- UIKitSliderInSwiftUI/ContentView.swift
+++ UIKitSliderInSwiftUI/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sliderValue: Float = 50
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            Text(String(format: "%.f", sliderValue))
+                .frame(width: 30)
+            UIKitSlider(value: $sliderValue)
+                .opacity(Double(sliderValue / 100))
+            Text(String(format: "%.f", sliderValue))
+                .frame(width: 30)
+        }
+        
     }
 }
 
---
+++ UIKitSliderInSwiftUI/UIKitSlider.swift
@@ -0,0 +1,56 @@
+//
+//  UIKitSlider.swift
+//  UIKitSliderInSwiftUI
+//
+//  Created by Повелитель on 08.11.2021.
+//
+
+import SwiftUI
+
+struct UIKitSlider: UIViewRepresentable {
+    @Binding var value: Float
+    
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider()
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.thumbTintColor = .black
+        slider.tintColor = .orange
+        slider.value = value
+        
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.didDragged),
+            for: .valueChanged)
+        
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.value = value
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(value: $value)
+    }
+}
+
+extension UIKitSlider {
+    class Coordinator: NSObject {
+        @Binding var value: Float
+        
+        init(value: Binding<Float>) {
+            self._value = value
+        }
+        
+        @objc func didDragged(_ sender: UISlider) {
+            value = roundf(sender.value)
+        }
+    }
+}
+
+struct UIKitSlider_Previews: PreviewProvider {
+    static var previews: some View {
+        UIKitSlider(value: .constant(50))
+    }
+}
