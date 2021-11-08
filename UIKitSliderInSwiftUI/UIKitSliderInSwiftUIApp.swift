1bd494b6f5d022590cd76dfddae690ba82e4aec8
Dmitri Gordeev <dimapushkin@gmail.com>
2021-11-08 17:42:37 +0300

    Initial Commit

---
+++ UIKitSliderInSwiftUI/UIKitSliderInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitSliderInSwiftUIApp.swift
+//  UIKitSliderInSwiftUI
+//
+//  Created by Повелитель on 08.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct UIKitSliderInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
